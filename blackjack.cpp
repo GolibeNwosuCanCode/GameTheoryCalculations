a8358118025789b94e77556f40a45baab8ca06b7
GolibeNwosuCanCode <gogosnwosu@gmail.com>
2026-06-10 08:43:58 -0400

    Added vector with card values and probabilities, so that we can simulate the randomness now. Next Iĺl need to begin the actual simulation

--- blackjack.cpp
+++ blackjack.cpp
@@ -7,6 +7,17 @@ struct ValueProbabilty{
 }
 int main(){
     long long numTrial{1000000};
+    std::vector<ValueProbability> cardValues[10];
+    for(int valued=2; valued<12; valued++){
+        if(valued==10){
+            cardValues[valued-2].value=10;
+            cardValues[valued-2].probability=16.0000/52.0000;
+        }
+        else {
+            cardValues[valued-2].value=valued;
+            cardValues[valued-2].probability=4.0000/52.0000;
+        }
+    }
     for(int dealerFaceUp=2; dealerFaceUp<12; dealerFaceUp++){
         for(int staySum=0; staySum<21; staySum++){
             
