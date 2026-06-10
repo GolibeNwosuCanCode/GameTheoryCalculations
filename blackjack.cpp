4cc1cdb9bc18f71e5701c59fc6c7c0e57942a578
GolibeNwosuCanCode <gogosnwosu@gmail.com>
2026-06-10 00:13:02 +0000

    set up blackjack for loops based on sum to stay at and dealer face up.

---
+++ blackjack.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc.++h>
+//for this I'll assume probability of drawing cards is constant even though its not
+struct ValueProbabilty{
+    int value;
+    float probability;
+}
+int main(){
+    long long numTrial{1000000};
+    for(int dealerFaceUp=2; dealerFaceUp<12; dealerFaceUp++){
+        for(int staySum=0; staySum<21; staySum++){
+            
+        }
+    }
+}
